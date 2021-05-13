4ba04fded18d38af1e3aa732e9069d7c0c7b77da
Werecat <littleowl9@yandex.ru>
2021-05-13 21:57:57 +0300

    Home work 7

--- Lesson7/2.py
+++ Lesson7/2.py
@@ -9,3 +9,38 @@
 классы для основных классов проекта, проверить на практике работу декоратора @property.
 """
 
+from abc import abstractmethod
+
+
+class Clothes:
+
+    @abstractmethod
+    def fabric_consumption(self):
+        pass
+
+
+class Coat(Clothes):
+
+    def __init__(self, size: int):
+        self.size = size
+
+    @property
+    def fabric_consumption(self):
+        return round(self.size / 6.5 + 0.5, 2)
+
+
+class Dress(Clothes):
+
+    def __init__(self, height: int):
+        self.height = height
+
+    @property
+    def fabric_consumption(self):
+        return round(2 * self.height + 0.3, 1)
+
+
+coat = Coat(13)
+print(coat.fabric_consumption)
+
+dress = Dress(180)
+print(dress.fabric_consumption)
